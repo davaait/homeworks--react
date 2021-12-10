e3841123d6bb2765a784cbd900dc6e4ae211ff9e
David Ait <davidait2996@gmail.com>
2021-12-10 13:57:49 +0300

    "added message box"

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type MessageType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+export function Message(props: MessageType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <div className={s.imageBlock}>
+                <img src={props.avatar} alt="ava"/>
+            </div>
+            <div className={s.messageWrapper}>
+                <div className={s.name}>
+                    {props.name}
+                </div>
+                <div>
+                    {props.message}
+                </div>
+                <div className={s.time}>
+                    {props.time}
+                </div>
+            </div>
         </div>
     )
 }
-
-export default Message
